67a522e88fc4774c1bd33edd5be19a436569634b
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-10-24 16:02:50 +0300

    Task1

---
+++ IntroductionToDbApps/01-InitialSetup/InitialSetup.cs
@@ -0,0 +1,104 @@
+﻿namespace InitialSetup
+{
+    using System;
+    using System.Data.SqlClient;
+    using System.IO;
+
+    public class InitialSetup
+    {
+        public const string connectionStringMaster = "Server=.;Database=master;Integrated Security = true;";
+
+        public const string connectionStringMinionsDB = "Server=.;Database=MinionsDB;Integrated Security = true;";
+
+        public const string databaseName = "MinionsDB";
+
+        public static void Main()
+        {
+            CreateDatabase();
+
+            CreateTables();
+
+            InsertEntries();
+        }
+
+        private static void CreateDatabase()
+        {
+            SqlConnection initialConnection = new SqlConnection(connectionStringMaster);
+
+            initialConnection.Open();
+
+            using (initialConnection)
+            {
+                try
+                {
+                    SqlCommand createDb = new SqlCommand($"CREATE DATABASE {databaseName}", initialConnection);
+
+                    createDb.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error during Database creation - " + e.Message);
+                }
+            }
+        }
+
+        private static void CreateTables()
+        {
+            SqlConnection connection = new SqlConnection(connectionStringMinionsDB);
+
+            connection.Open();
+
+            using (connection)
+            {
+                using (StreamReader sr = new StreamReader("CreateTables.txt"))
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        string commandText = sr.ReadLine();
+                        int nameEnd = commandText.IndexOf('(') - 1;
+                        string tableName = commandText.Substring(13, nameEnd - 13);
+
+                        try
+                        {
+                            SqlCommand createTable = new SqlCommand(commandText, connection);
+                            createTable.ExecuteNonQuery();
+                            Console.WriteLine($"Table '{tableName}' successfully created.");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Creating table '{tableName}' failed - {e.Message}");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void InsertEntries()
+        {
+            SqlConnection connection = new SqlConnection(connectionStringMinionsDB);
+
+            connection.Open();
+
+            using (connection)
+            {
+                using (StreamReader reader = new StreamReader("TablesEntries.txt"))
+                {
+                    while (reader.EndOfStream == false)
+                    {
+                        try
+                        {
+                            string commandText = reader.ReadLine();
+                            SqlCommand insert = new SqlCommand(commandText, connection);
+                            int rowsAffected = insert.ExecuteNonQuery();
+                            Console.WriteLine($"{rowsAffected} rows affected.");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Inserting failed - {e.Message}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
