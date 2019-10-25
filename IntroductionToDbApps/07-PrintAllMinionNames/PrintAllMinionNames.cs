e8557f4b96c7a620bb3ee0679021aa2cdbc99cd6
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-10-25 14:00:21 +0300

    Task7

---
+++ IntroductionToDbApps/07-PrintAllMinionNames/PrintAllMinionNames.cs
@@ -0,0 +1,41 @@
+﻿namespace PrintAllMinionNames
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+
+    public class PrintAllMinionNames
+    {
+        public static void Main()
+        {
+            string connectionString = "Server=.;Database=MinionsDB;Integrated Security = true;";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            using (connection)
+            {
+                SqlCommand getNames = new SqlCommand("SELECT Name FROM Minions", connection);
+                SqlDataReader reader = getNames.ExecuteReader();
+
+                List<string> names = new List<string>();
+                while (reader.Read())
+                {
+                    names.Add((string)reader[0]);
+                }
+
+                for (int i = 0; i < names.Count / 2; i++)
+                {
+                    Console.WriteLine(names[i]);
+                    Console.WriteLine(names[names.Count - 1 - i]);
+                }
+
+                if (names.Count % 2 != 0)
+                {
+                    Console.WriteLine(names[names.Count / 2]);
+                }
+            }
+        }
+    }
+}
