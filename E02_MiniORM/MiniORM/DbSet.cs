c4839a00df7ae5ae9e40b429f07705178fe08115
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-10-29 11:08:15 +0200

    MiniORM Library

---
+++ E02_MiniORM/MiniORM/DbSet.cs
@@ -0,0 +1,89 @@
+﻿namespace MiniORM
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DbSet<TEntity> : ICollection<TEntity>
+        where TEntity : class, new()
+    {
+        public DbSet(IEnumerable<TEntity> entities)
+        {
+            this.Entities = entities.ToList();
+            this.ChangeTracker = new ChangeTracker<TEntity>(entities);
+        }
+
+        internal ChangeTracker<TEntity> ChangeTracker {get; set;}
+
+        internal IList<TEntity> Entities { get; set; }
+
+        public void Add(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null");
+            }
+
+            this.Entities.Add(item);
+            this.ChangeTracker.Add(item);
+        }
+
+        public void Clear()
+        {
+            while (this.Entities.Any())
+            {
+                var entity = this.Entities.First();
+                this.Remove(entity);
+            }
+        }
+        public bool Contains(TEntity item)
+        {
+            return this.Entities.Contains(item);
+        }
+
+        public void CopyTo(TEntity[] array, int startIndex)
+        {
+            this.Entities.CopyTo(array, startIndex);
+        }
+
+        public int Count => this.Entities.Count();
+
+        public bool IsReadOnly => this.Entities.IsReadOnly;
+
+        public bool Remove(TEntity item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null");
+            }
+
+            bool removedSuccessfully = this.Entities.Remove(item);
+
+            if (removedSuccessfully)
+            {
+                this.ChangeTracker.Remove(item);
+            }
+
+            return removedSuccessfully;
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities.ToArray())
+            {
+                this.Remove(entity);
+            }
+        }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return this.Entities.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ E02_MiniORM/MiniORM/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniORM
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+    }
+}
