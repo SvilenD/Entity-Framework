0e3bf0382881f9936bbbbee51b9fb21195543feb
SvilenD <42715140+SvilenD@users.noreply.github.com>
2019-10-30 13:31:34 +0200

    Task4

---
+++ E03_IntroductionToEntityFramework/P04_EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var db = new SoftUniContext();
+
+            var result = GetEmployeesWithSalaryOver50000(db);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder result = new StringBuilder();
+
+            var employees = context.Employees.
+                Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName);
+
+            foreach (var employee in employees)
+            {
+                result.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
